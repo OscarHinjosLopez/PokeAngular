de64c83cec215bad707eadc4f80040071217eca9
Oscar <hinjoslopezoscar@gmail.com>
2024-10-05 21:34:53 +0200

    Updates

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-7R3O3RYS.mjs";import"./chunk-23H5RS3J.mjs";import"./chunk-WN55FAD5.mjs";import"./chunk-EPORDKSO.mjs";import"./chunk-XILAMP3A.mjs";import"./chunk-OP7IP4LQ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
